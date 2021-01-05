bd7e60e859089466588964828fce377b90fa41f6
강상원 <kevink1113@u.sogang.ac.kr>
2021-01-05 19:19:58 +0900

    Dust_semi-fin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import xml2js from "xml2js";
+import Dust from "./Dust";
 import axios from "axios";
 
 class App extends React.Component {
@@ -10,27 +10,20 @@ class App extends React.Component {
     };
 
     getDust() {
-        let ress = null;
+        console.log(process.env.REACT_APP_GOOGLE_API_KEY);
         const API_KEY = process.env.REACT_APP_API_KEY;
-        const url = `/openapi/services/rest/MsrstnInfoInqireSvc/getNearbyMsrstnList?tmX=244148.546388&tmY=412423.75772&ServiceKey=${API_KEY}`;
-        console.log(url);
-        var results;
-        axios
-            .get(url, { "Content-Type": "application/xml; charset=utf-8" })
-            .then((response) => {
-                let res2 = response.data;
-                let parser = new xml2js.Parser();
-                parser.parseString(res2, (err, result) => {
-                    JSON.parse(JSON.stringify(result));
-                    console.log(result);
-                    // setState({ isLoading: false });
-                });
-                console.log(res2);
+        const url = `/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=송파구&dataTerm=month&pageNo=1&numOfRows=10&_returnType=json&ServiceKey=${API_KEY}&ver=1.3`;
+        // console.log(url);
+        axios.get(url).then((response) => {
+            const data = response.data.list[0];
+            console.log(data);
+
+            this.setState({
+                isLoading: false,
+                data: data,
             });
-
-        console.log(results);
+        });
     }
-
     componentDidMount() {
         this.getDust();
     }
@@ -38,7 +31,31 @@ class App extends React.Component {
     render() {
         const { isLoading, data } = this.state;
 
-        return <div>HELLO!</div>;
+        return (
+            <section className="container">
+                {isLoading ? (
+                    <div className="loader">
+                        <span className="loader_text">Loading...</span>
+                    </div>
+                ) : (
+                    <div className="dustInfo">
+                        <Dust
+                            date={data.dataTime}
+                            pm10val={data.pm10Value}
+                            pm10gr={data.pm10Grade}
+                            pm25val={data.pm25Value}
+                            pm25gr={data.pm25Grade}
+                            o3val={data.o3Value}
+                            o3gr={data.o3Grade}
+                            coval={data.coValue}
+                            cogr={data.coGrade}
+                            no2val={data.no2Value}
+                            no2gr={data.no2Grade}
+                        />
+                    </div>
+                )}
+            </section>
+        );
     }
 }
 
