46f5032694782cb64f13f2545185ec22f4f96ec7
강상원 <kevink1113@u.sogang.ac.kr>
2021-02-18 17:52:31 +0900

    baseURL

--- src/App.js
+++ src/App.js
@@ -1,62 +1,63 @@
-import React from "react";
-import "./App.css";
-import Dust from "./Dust";
-import axios from "axios";
+import React from 'react';
+import './App.css';
+import Dust from './Dust';
+import axios from 'axios';
 
 class App extends React.Component {
-    state = {
-        isLoading: true,
-        data: {},
-    };
+	state = {
+		isLoading: true,
+		data: {},
+	};
 
-    getDust() {
-        console.log(process.env.REACT_APP_GOOGLE_API_KEY);
-        const API_KEY = process.env.REACT_APP_API_KEY;
-        const url = `/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=송파구&dataTerm=month&pageNo=1&numOfRows=10&_returnType=json&ServiceKey=${API_KEY}&ver=1.3`;
-        // console.log(url);
-        axios.get(url).then((response) => {
-            const data = response.data.list[0];
-            console.log(data);
+	getDust() {
+		const API_KEY = process.env.REACT_APP_API_KEY;
+		const url = `/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=송파구&dataTerm=month&pageNo=1&numOfRows=10&_returnType=json&ServiceKey=${API_KEY}&ver=1.3`;
+		// console.log(url);
+		axios.defaults.baseURL =
+			process.env.NODE_ENV === 'development' ? '/' : 'http://openapi.airkorea.or.kr/';
+		axios.get(url).then((response) => {
+			const data = response.data.list[0];
+			console.log(data);
 
-            this.setState({
-                isLoading: false,
-                data: data,
-            });
-        });
-    }
-    componentDidMount() {
-        this.getDust();
-    }
+			this.setState({
+				isLoading: false,
+				data: data,
+			});
+		});
+	}
+	componentDidMount() {
+		this.getDust();
+	}
 
-    render() {
-        const { isLoading, data } = this.state;
+	render() {
+		const { isLoading, data } = this.state;
 
-        return (
-            <section className="container">
-                {isLoading ? (
-                    <div className="loader">
-                        <span className="loader_text">Loading...</span>
-                    </div>
-                ) : (
-                    <div className="dustInfo">
-                        <Dust
-                            date={data.dataTime}
-                            pm10val={data.pm10Value}
-                            pm10gr={data.pm10Grade}
-                            pm25val={data.pm25Value}
-                            pm25gr={data.pm25Grade}
-                            o3val={data.o3Value}
-                            o3gr={data.o3Grade}
-                            coval={data.coValue}
-                            cogr={data.coGrade}
-                            no2val={data.no2Value}
-                            no2gr={data.no2Grade}
-                        />
-                    </div>
-                )}
-            </section>
-        );
-    }
+		return (
+			<section className="container">
+				{isLoading ? (
+					<div className="loader">
+						<span className="loader_text">Loading...</span>
+					</div>
+				) : (
+					<div className="dustInfo">
+						<Dust
+							date={data.dataTime}
+							pm10val={data.pm10Value}
+							pm10gr={data.pm10Grade}
+							pm25val={data.pm25Value}
+							pm25gr={data.pm25Grade}
+							o3val={data.o3Value}
+							o3gr={data.o3Grade}
+							coval={data.coValue}
+							cogr={data.coGrade}
+							no2val={data.no2Value}
+							no2gr={data.no2Grade}
+						/>
+					</div>
+				)}
+			</section>
+		);
+	}
 }
 
 export default App;
